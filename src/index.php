e415ca1fa9906eb405f7218ce69a0049b5efdbec
Vinicius Moreira <vinicius.ads.mec@gmail.com>
2017-07-02 13:38:56 -0300

    teste1

--- src/index.php
+++ src/index.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
 <html lang="pt-br">
     <head>
-        <meta charset="UTF-8" />>
+        <meta charset="UTF-8" />
         <title>Nexus - Hello New PC Game</title>
     </head>
     <body>
-
     </body>
 </html>
