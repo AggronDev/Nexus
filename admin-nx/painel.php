4a49038221313ed66c82237d9423e57b7cafea76
Vinicius Moreira <vinicius.ads.mec@gmail.com>
2017-07-03 09:13:44 -0300

    ordenando

---
+++ admin-nx/painel.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8" />>
+        <title>Nexus - Administração</title>
+    </head>
+    <body>
+        <div id="dvPainel">
+            
+        </div>
+    
+    </body>
+</html>
